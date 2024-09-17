c347e6854fc6686ff50e36809ee55e51c528b515
Heitorccf <Heitorccfernandes550@gmail.com>
2024-09-17 14:17:46 -0300

    Mais Funções

--- Pipeline/Documentos/Setores/scripts/merge.py
+++ Pipeline/Documentos/Setores/scripts/merge.py
@@ -1,6 +1,5 @@
 import json
-
-path_json="Pipeline/Documentos/Setores/unprocessed-data/files-comp-I.json"
+import csv
 
 def leitura_json(path_json):
    dados_json=[]
@@ -8,5 +7,19 @@ def leitura_json(path_json):
       dados_json=json.load(file)
    return dados_json
 
+def leitura_csv(path_csv):
+   dados_csv=[]
+   with open(path_csv, "r") as file:
+      spamreader=csv.DictReader(file, delimiter=",")
+      for row in spamreader:
+         dados_csv.append(row)
+   return dados_csv
+
+path_json="Pipeline/Documentos/Setores/unprocessed-data/files-comp-I.json"
+path_csv="Pipeline/Documentos/Setores/unprocessed-data/files-comp-II.csv"
+
 dados_json=leitura_json(path_json)
 print(dados_json[0])
+
+dados_csv=leitura_csv(path_csv)
+print(dados_csv[0])
